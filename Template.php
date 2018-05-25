34a989439fef8389a7d77eeb4f1d8c35bd8a7ace
Pakpoom@SUT <wisedsri2@gmail.com>
2018-05-25 11:50:58 +0700

    Update Template.php

--- Template.php
+++ Template.php
@@ -1,20 +1,20 @@
 
 <!DOCTYPE html>
 
-	<?php /* header_top */ include "views/header_top.php"; ?>
+	<?php /* header_top */include "views/header_top.php";?>
 	<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 	<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 	<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 	<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 	<head>
-        
+
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
+
     <title> .: TACE Activity :. </title>
 
 
-    <?php /* header_meta */ include "views/meta.php"; ?>
+    <?php /* header_meta */include "views/meta.php";?>
 
 	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
 	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
@@ -30,30 +30,30 @@
 	<meta name="twitter:image" content="" />
 	<meta name="twitter:url" content="" />
     <meta name="twitter:card" content="" />
-    
-    <?php /* header_assets */ include "views/header_assets.php"; ?>
+
+    <?php /* header_assets */include "views/header_assets.php";?>
 
 	<body>
 		<div id="fh5co-wrapper">
 		<div id="fh5co-page">
 
-		<?php /* header_nav */ include "views/header_nav.php"; ?>
-		
-		<!-- Put Each Section Here -->
-
-        
-
+		<?php /* header_nav */include "views/header_nav.php";?>
 
 		<!-- Put Each Section Here -->
 
-		<?php /* footer_ui */ include "views/footer_ui.php"; ?>
+
+
+
+		<!-- Put Each Section Here -->
+
+		<?php /* footer_ui */include "views/footer_ui.php";?>
 
 	</div>
 	<!-- END fh5co-page -->
 	</div>
 	<!-- END fh5co-wrapper -->
 
-    <?php /* footer */ include "views/footer.php"; ?>
+    <?php /* footer */include "views/footer.php";?>
 
 	</body>
 </html>
