a1ea7236789d27699b91cd0afb8378cdf5b7b748
Pakpoom@SUT <wisedsri2@gmail.com>
2018-05-26 10:07:01 +0700

    เพิ่ม Blog ในหน้า index.php เปลี่ยน Nav Header

---
+++ index.php
@@ -0,0 +1,126 @@
+
+<!DOCTYPE html>
+
+<?php /* header_top */include "views/header_top.php";?>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<title> .: TACE Activity :. </title>
+
+
+<?php /* header_meta */include "views/meta.php";?>
+
+<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+<meta name="author" content="FREEHTML5.CO" />
+
+<!-- Facebook and Twitter integration -->
+<meta property="og:title" content=""/>
+<meta property="og:image" content=""/>
+<meta property="og:url" content=""/>
+<meta property="og:site_name" content=""/>
+<meta property="og:description" content=""/>
+<meta name="twitter:title" content="" />
+<meta name="twitter:image" content="" />
+<meta name="twitter:url" content="" />
+<meta name="twitter:card" content="" />
+
+<?php /* header_assets */include "views/header_assets.php";?>
+
+    <body>
+	<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+
+			<?php /* header_nav */include "views/header_nav.php"; ?>
+
+			<!-- Put Each Section Here -->
+
+			<div id="fh5co-blog-section" class="fh5co-section-gray">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box fadeInUp animated">
+							<h3>กิจกรรมที่กำลังจะมาถึง</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row row-bottom-padded-md">
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="fh5co-blog animate-box fadeInUp animated">
+								<a href="#"><img class="img-responsive" src="images/cover_bg_1.jpg" alt=""></a>
+								<div class="blog-text">
+									<div class="prod-title">
+										<h3><a href="" #="">หลักสูตรการอบรม Medical Mission in Southern Kenya</a></h3>
+										<span class="posted_by">Sep. 15th</span>
+										<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+										<p><a href="#">ลงทะเบียน</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="fh5co-blog animate-box fadeInUp animated">
+								<a href="#"><img class="img-responsive" src="images/cover_bg_2.jpg" alt=""></a>
+								<div class="blog-text">
+									<div class="prod-title">
+										<h3><a href="" #="">หลักสูตรการอบรม Medical Mission in Southern Kenya</a></h3>
+										<span class="posted_by">Sep. 15th</span>
+										<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+										<p><a href="#">ลงทะเบียน</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="clearfix visible-sm-block"></div>
+						<div class="col-lg-4 col-md-4 col-sm-6">
+							<div class="fh5co-blog animate-box fadeInUp animated">
+								<a href="#"><img class="img-responsive" src="images/cover_bg_3.jpg" alt=""></a>
+								<div class="blog-text">
+									<div class="prod-title">
+										<h3><a href="" #="">หลักสูตรการอบรม Medical Mission in Southern Kenya</a></h3>
+										<span class="posted_by">Sep. 15th</span>
+										<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+										<p><a href="#">ลงทะเบียน</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="clearfix visible-md-block"></div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-4 col-md-offset-4 text-center animate-box fadeInUp animated">
+							<a href="#" class="btn btn-primary btn-lg"> Register </a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+
+			<!-- Put Each Section Here -->
+
+			<?php /* footer_ui */include "views/footer_ui.php";?>
+
+		</div>
+		<!-- END fh5co-page -->
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<?php /* footer */include "views/footer.php";?>
+
+</body>
+    
+
+</html>
+
