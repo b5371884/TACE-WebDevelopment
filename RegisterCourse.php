0a6aabda99729f86b745dbd05c1a0a7d8c637077
Stoppu <stoppu@MacBook-Pro-khxng-Stoppu.local>
2018-05-26 18:46:37 +0700

    add button to payment

--- RegisterCourse.php
+++ RegisterCourse.php
@@ -601,8 +601,9 @@ include "views/header_top.php"; ?>
                                 <div class="form-group">
                                     <label class="col-md-4 control-label" for="ButtonSave">ยืนยันการลงทะเบียน</label>
                                     <div class="col-md-8">
-                                        <button id="ButtonSave" name="ButtonSave" class="btn btn-success">บันทึกข้อมูล
-                                        </button>
+                                        <a href="PaymentMethod.php.php" class="btn btn-success btn-lg"> ลงทะเบียน </a>
+<!--                                        <button id="ButtonSave" name="ButtonSave" class="btn btn-success">บันทึกข้อมูล-->
+<!--                                        </button>-->
                                         <button id="ButtonCancle" name="ButtonCancle" class="btn btn-danger">ยกเลิก
                                         </button>
                                     </div>
