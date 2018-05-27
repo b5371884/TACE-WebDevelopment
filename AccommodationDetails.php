626ce85a4c1f500f4cc94978477cc0ddb4159862
stoppustoppy <stoppustoppy@gmail.com>
2018-05-27 15:13:38 +0700

    Accommodation room photo

--- AccommodationDetails.php
+++ AccommodationDetails.php
@@ -1,78 +1,102 @@
-
 <!DOCTYPE html>
 
-	<?php /* header_top */include "views/header_top.php";?>
-	<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-	<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-	<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-	<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
+<?php /* header_top */
+include "views/header_top.php"; ?>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
 
-	<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <title> .: การเดินทางและที่พัก : TACE Activity :. </title>
 
 
-    <?php /* header_meta */include "views/meta.php";?>
+    <?php /* header_meta */
+    include "views/meta.php"; ?>
 
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FREEHTML5.CO" />
+    <meta name="description" content="Free HTML5 Template by FREEHTML5.CO"/>
+    <meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive"/>
+    <meta name="author" content="FREEHTML5.CO"/>
 
     <!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-    <meta name="twitter:card" content="" />
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content=""/>
+    <meta name="twitter:image" content=""/>
+    <meta name="twitter:url" content=""/>
+    <meta name="twitter:card" content=""/>
 
-    <?php /* header_assets */include "views/header_assets.php";?>
+    <?php /* header_assets */
+    include "views/header_assets.php"; ?>
 
-	<body>
-		<div id="fh5co-wrapper">
-		<div id="fh5co-page">
+<body>
+<div id="fh5co-wrapper">
+    <div id="fh5co-page">
 
-		<?php /* header_nav */include "views/header_nav.php";?>
+        <?php /* header_nav */
+        include "views/header_nav.php"; ?>
 
-		<!-- Put Each Section Here -->
+        <!-- Put Each Section Here -->
 
         <div id="fh5co-content-section" class="fh5co-section-gray">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12 text-center heading-section animate-box fadeInUp animated">
-						<h3>ที่พักและการเดินทาง</h3>
-						<p> ห้องมิตรภาพ </p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center heading-section animate-box fadeInUp animated">
+                        <h3>ที่พักและการเดินทาง</h3>
+                        <p> ห้องมิตรภาพ </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                       <p>ห้องมิตรภาพ ห้องประชุมขนาดกลางรองลงมาจากห้องไทรงาม สามารถจุคนได้ 120 คน
-                       <br/>เหมาะสำหรับงานวิชาการ หรืองานจัดเลี้ยงที่มีขนาดปานกลาง
-                       <br/>ห้องประชุมสามารถเลือกสไตล์การตกแต่งได้ตามที่ลูกค้าต้องการ 
-                       <br/>ด้วยทีมงานออแกไนซ์มืออาชีพของเรา 
-                       <br/>ลูกค้าสามารถเลือกอาหารหรือเครื่องดื่ม
-                       <br/>ในการจัดงานตามความต้องการของท่านเองได้อย่างง่ายดาย</p>
-        
-                        <img src="https://www.simathani.com/uploads/6075/images/meeting_and_conference/Mittraphap.jpg" width="800px">
-                        
-					</div>
-				</div>
-			</div>
-		</div>
+        <div id="fh5co-content-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center animate-box">
+                        <p>ห้องมิตรภาพ ห้องประชุมขนาดกลางรองลงมาจากห้องไทรงาม สามารถจุคนได้ 120 คน
+                            <br/>เหมาะสำหรับงานวิชาการ หรืองานจัดเลี้ยงที่มีขนาดปานกลาง
+                            <br/>ห้องประชุมสามารถเลือกสไตล์การตกแต่งได้ตามที่ลูกค้าต้องการ
+                            <br/>ด้วยทีมงานออแกไนซ์มืออาชีพของเรา
+                            <br/>ลูกค้าสามารถเลือกอาหารหรือเครื่องดื่ม
+                            <br/>ในการจัดงานตามความต้องการของท่านเองได้อย่างง่ายดาย</p>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center animate-box">
+                    <img src="https://www.simathani.com/uploads/6075/images/meeting_and_conference/Mittraphap.jpg"
+                         alt="Accommodation room" width="800px" class="img-responsive">
+                </div>
+                <div class="col-md-6 text-center animate-box">
+                    <img src="https://www.simathani.com/uploads/6075/images/meeting_and_conference/Mittraphap.jpg"
+                         alt="Accommodation room" width="800px" class="img-responsive">
+                </div>
 
-		<!-- Put Each Section Here -->
+            </div>
+        </div>
 
-		<?php /* footer_ui */include "views/footer_ui.php";?>
 
-	</div>
-	<!-- END fh5co-page -->
-	</div>
-	<!-- END fh5co-wrapper -->
+        <!-- Put Each Section Here -->
 
-    <?php /* footer */include "views/footer.php";?>
+        <?php /* footer_ui */
+        include "views/footer_ui.php"; ?>
 
-	</body>
+    </div>
+    <!-- END fh5co-page -->
+</div>
+<!-- END fh5co-wrapper -->
+
+<?php /* footer */
+include "views/footer.php"; ?>
+
+</body>
 </html>
 
